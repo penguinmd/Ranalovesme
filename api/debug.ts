c719727bb6f1f5cc16d3333e5ac316cf43304a74
penguinmd <myfourkids@gmail.com>
2025-10-19 10:38:22 -0700

    Show INIT_SECRET preview in debug endpoint

--- api/debug.ts
+++ api/debug.ts
@@ -4,6 +4,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   return res.status(200).json({
     hasInitSecret: !!process.env.INIT_SECRET,
     initSecretLength: process.env.INIT_SECRET?.length || 0,
+    initSecretPreview: process.env.INIT_SECRET?.substring(0, 10) + '...' || 'none',
     hasJwtSecret: !!process.env.JWT_SECRET,
     hasPostgresUrl: !!process.env.POSTGRES_URL,
     hasBlobToken: !!process.env.BLOB_READ_WRITE_TOKEN,
